2fd51bb2472fc55f604084ae8cf62bfa75bf169b
Alex Shatokhin <shatokhin.04@inbox.ru>
2024-06-10 19:54:41 +0300

    add socket io server

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const { createServer } = require('node:http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
+server.listen(process.env.PORT, () => {
+  console.log('server running at http://localhost:' + process.env.PORT);
+});
